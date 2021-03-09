8628eb209d2f57048dafb53acac37d86446563cc
Success Ologunsua <successologunsua@gmail.com>
2021-03-09 10:11:12 +0100

    changes

--- Node/user-input-ass/app.js
+++ Node/user-input-ass/app.js
@@ -56,24 +56,6 @@ app.get("/signin", function (req, res) {
 });
 
 
-app.patch("/userDetails/:id", function (req, res) {
-  // const { id } = req.params;
-  // const user = userDetails.find((el) => el.email === id);
-  // if (user) {
-  //   if (user.email !== req.body.email){
-  //     res.status(400).json({ status: "fail", message: "email not updated"})
-  //   }
-  //     user.firstName = req.body.firstName
-  //     user.lastName = req.body.lastName
-  //     user.password = req.body.password      
-  //     return res.status(200).json({
-  //         status: "success",
-  //         message: "Email updated",
-  //         data: user
-  //     });
-  // }
-  // res.status(400).json({ status: "fail", message: "Email not updated "})
-})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
